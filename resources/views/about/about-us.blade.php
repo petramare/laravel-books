35668bdd0c15d8624cd7a47f3deb43fd246ccaf0
Jan Polak <jan.polak@onmyway.cz>
2023-02-27 15:02:01 +0100

    pages using layouts & sections

---
+++ resources/views/about/about-us.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main', [
+    'current_menu_item' => 'about-us'
+])
+
+@section('content')
+
+    <h1>About us</h1>
+
+    <p>
+        We are the greatest team of librarians that you've ever met.
+    </p>
+
+@endsection
---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main', [
+    'current_menu_item' => 'homepage'
+])
+
+@section('content')
+
+    <h1>Laravel Books</h1>
+
+    <p>
+        We are the best online bookstore ever.
+        You will keep coming back for more.
+    </p>
+
+@endsection
